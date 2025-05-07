86691437ef09ae9b6195793003bfd32b0eaf0000
Konrad Michalik <km@move-elevator.de>
2025-05-07 15:50:09 +0200

    refactor: add feature:init dependency for feature:notify in deployment process

--- deployer/feature/task/deploy.php
+++ deployer/feature/task/deploy.php
@@ -10,6 +10,7 @@ before('rollback', 'feature:init');
 before('deploy:unlock', 'feature:init');
 before('feature:sync', 'feature:init');
 before('deploy:setup', 'feature:setup');
+before('feature:notify', 'feature:init');
 before('deploy:success', 'feature:notify');
 after('deploy:clear_paths', 'feature:sync');
 after('deploy:symlink', 'feature:urlshortener');
